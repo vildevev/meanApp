38d7994cc6eefee1167a58c3231c93ec53a18aae
“Vilde <vilde@vevatne.no>
2017-05-01 00:15:37 -0700

    Rewrites for new angular version

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp');
 
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   $scope.getBooks = function(){
-    $http.get('/api/books').success(function(response){
+    $http.get('/api/books').then(function(response){
       $scope.books = response;
     });
   }
