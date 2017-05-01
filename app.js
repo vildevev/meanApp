7eb3f4d975adb3b5df307db7342750a83cfc155b
“Vilde <vilde@vevatne.no>
2017-04-30 18:52:22 -0700

    Adds message to response.send

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
+
+app.get('/', function(req, res){
+  res.send("Please use /api/books or /api/genres");
+});
+
+app.listen(3000);
+console.log("Running on port 3000");
