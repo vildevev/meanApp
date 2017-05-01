872d853fe8164bbfa803b863f96a0d1dbe542698
“Vilde <vilde@vevatne.no>
2017-05-01 01:31:13 -0700

    Adds route for book details

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -6,4 +6,11 @@ myApp.controller('BooksController', ['$scope', '$http', '$location', '$routePara
       $scope.books = response.data;
     });
   }
+
+  $scope.getBook = function(){
+    var id = $routeParams.id;
+    $http.get('/api/books/'+id).then(function(response){
+      $scope.books = response.data;
+    });
+  }
 }]);
