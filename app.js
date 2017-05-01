9eb169ec2fd453bf4bb94a7990b5d6f69f049d12
“Vilde <vilde@vevatne.no>
2017-04-30 19:43:18 -0700

    Creates /api/genres route

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+Genre = require('./models/genre');
+
+// Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
 
 app.get('/', function(req, res){
   res.send("Please use /api/books or /api/genres");
+});
+
+app.get('/api/genres', function(req, res){
+  Genre.getGenres(function(err, genres){
+    if(err){
+      throw err;
+    }
+    res.json(genres);
+  })
+
 });
 
 app.listen(3000);
