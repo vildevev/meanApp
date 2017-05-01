a76396fc1908e2fc9c1b9f5b626733bcc0dac56a
“Vilde <vilde@vevatne.no>
2017-05-01 01:12:07 -0700

    Fixes book display

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp');
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   $scope.getBooks = function(){
     $http.get('/api/books').then(function(response){
-      $scope.books = response;
+      $scope.books = response.data;
     });
   }
 }]);
