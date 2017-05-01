a6b709c72e7cd8c5002c8ad2836f49a579babf1a
“Vilde <vilde@vevatne.no>
2017-04-30 20:01:25 -0700

    Adds route for /api/books

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-Genre = require('./models/genre');
+Genre = require('./models/genre.js');
+Book = require('./models/book.js');
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
@@ -19,9 +20,17 @@ app.get('/api/genres', function(req, res){
       throw err;
     }
     res.json(genres);
-  })
+  });
+  });
 
-});
+  app.get('/api/books', function(req, res){
+    Book.getBooks(function(err, books){
+      if(err){
+        throw err;
+      }
+      res.json(books);
+    });
+  }); 
 
 app.listen(3000);
 console.log("Running on port 3000");
